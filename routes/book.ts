dfeb14366d996e51ca43ee3b6a702920c7ef1730
Elie Haddad <elie.g.haddad52@gmail.com>
2021-05-16 17:22:04 +0300

    Heroku

--- routes/book.ts
+++ routes/book.ts
@@ -7,12 +7,13 @@ import {
     updateBook,
 } from "../controllers/book";
 import isAuth from "../middleware/is_auth";
+import isRegisteredInDB from "../middleware/is_registered_in_db";
 
 const router = Router();
 
 router.get("/:objectID", isAuth, getBookById);
 router.get("/", isAuth, getBooks);
-router.post("/", isAuth, addBook);
+router.post("/", isAuth, isRegisteredInDB, addBook);
 router.put("/:objectID", isAuth, updateBook);
 router.delete("/:objectID", isAuth, deleteBook);
 
